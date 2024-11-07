2d707fe7fc789337e6be7f6a2bceecb7a5fb06a8
emmanueldev247 <mailemmydee@gmail.com>
2024-11-07 18:30:09 +0000

    updated: Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,27 @@ Regex-ing: This module contains functions to securely handle and
            obfuscate sensitive data within log messages.
 """
 import re
+import logging
 from typing import List
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Filter values in incoming log records using filter_datum"""
+        return filter_datum(self.fields, RedactingFormatter.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            RedactingFormatter.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str,
