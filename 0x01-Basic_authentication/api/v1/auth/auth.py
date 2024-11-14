40da320dc457964be4359eb7fc14dbc7cb311fa5
emmanueldev247 <mailemmydee@gmail.com>
2024-11-14 18:48:33 +0000

    updated: Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -22,7 +22,10 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """public method"""
-        return None
+        print(request)
+        if not request or "Authorization" not in request.headers:
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """public method"""
