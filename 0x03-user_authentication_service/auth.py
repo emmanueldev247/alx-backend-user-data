0b4ca4c08fc0dc9fca10e6258165c9b3e09d2201
emmanueldev247 <mailemmydee@gmail.com>
2024-11-22 22:13:40 +0000

    added: Hash password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Auth module
+"""
+
+import bcrypt
+def _hash_password(password: str) -> bytes:
+    """Function to hash password
+
+    Args:
+        password (str): password string to hash
+
+    Return: Salted hash of the input password
+    """
+    salt = bcrypt.gensalt()
+    encoded_pwd = password.encode('utf-8')
+    return bcrypt.hashpw(encoded_pwd, salt)
