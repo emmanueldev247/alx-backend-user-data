d0b69c7f730b9708fa9a7bceceb068976e5bb20a
emmanueldev247 <mailemmydee@gmail.com>
2024-11-23 18:55:06 +0000

    updated: annotation

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -74,7 +74,7 @@ def profile():
 
 
 @app.route("/reset_password", methods=["GET"], strict_slashes=False)
-def get_reset_password_token():
+def get_reset_password_token() -> str:
     """route handler for POST "/reset_password"
     """
     email = request.form.get("email")
