ac877f7235ddc1086f275cf49d1ffbfe2d14aefd
emmanueldev247 <mailemmydee@gmail.com>
2024-11-23 17:19:00 +0000

    added: Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -30,5 +30,29 @@ def users():
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """route handler for "/sessions"
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if not (email and password):
+        abort(400)
+
+    if not self.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+
+    return response, 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
